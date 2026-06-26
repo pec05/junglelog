51e43d603f96a9bad91c76ab73c8bce40b33c271
pec05 <leandro-peccio@hotmail.com>
2026-06-26 11:37:43 +0200

    Initial commit

---
+++ src/main/java/com/peccio/junglelog/JunglelogApplication.java
@@ -0,0 +1,13 @@
+package com.peccio.junglelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunglelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunglelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peccio/junglelog/JunglelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peccio.junglelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunglelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
